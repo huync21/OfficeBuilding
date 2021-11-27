30d3dbd045f54867110f9050952643ee98e4256f
huy <huync21@gmail.com>
2021-11-27 14:40:58 +0700

    full crud floor

--- src/main/java/com/example/officebuilding/controller/FloorController.java
+++ src/main/java/com/example/officebuilding/controller/FloorController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping(value = "/floors", produces = "application/json")
@@ -22,5 +24,24 @@ public class FloorController {
     @GetMapping
     public ResponseEntity<Iterable<FloorEntity>> getAllFloors() {
         return new ResponseEntity<>(floorEntityService.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FloorEntity> updateFloor(@PathVariable Integer id,@RequestBody FloorEntity floorEntity){
+        Optional<FloorEntity> floorEntityOptional = floorEntityService.findById(id);
+
+        return floorEntityOptional.map(floorEntity1 -> {
+            floorEntity.setId(floorEntity1.getId());
+            return new ResponseEntity<>(floorEntityService.save(floorEntity),HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<FloorEntity> deleteCategory(@PathVariable Integer id) {
+        Optional<FloorEntity> floorEntityOptional = floorEntityService.findById(id);
+        return floorEntityOptional.map(floorEntity -> {
+            floorEntityService.remove(id);
+            return new ResponseEntity<>(floorEntity, HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }

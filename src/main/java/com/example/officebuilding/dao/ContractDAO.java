2574e6b11cc102ebca286d28aef2db141cb7386f
hung-cell <daoquanghung922@gmail.com>
2021-12-08 13:46:53 +0700

    update contract module

---
+++ src/main/java/com/example/officebuilding/dao/ContractDAO.java
@@ -0,0 +1,36 @@
+package com.example.officebuilding.dao;
+
+import com.example.officebuilding.dtos.ContractDTO;
+import com.example.officebuilding.dtos.ServiceContractDTO;
+import com.example.officebuilding.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
+import org.springframework.stereotype.Repository;
+
+import java.text.SimpleDateFormat;
+
+@Repository
+public class ContractDAO {
+
+    public void createContract(Integer companyId, Integer floorId, ContractDTO contractDTO){
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            NativeQuery query = session.createSQLQuery("INSERT INTO contract(description,expired_date,is_canceled,rented_area,rented_date,company_id,floor_id) VALUES(:description,:expiredDate,:isCanceled,:rentedArea,:rentedDate,:companyId,:floorId)");
+            query.setParameter("companyId", companyId);
+            query.setParameter("floorId",floorId);
+            query.setParameter("expiredDate",contractDTO.getExpiredDate());
+            query.setParameter("rentedDate",contractDTO.getRentedDate());
+            query.setParameter("description",contractDTO.getDescription());
+            query.setParameter("isCanceled",contractDTO.getIsCanceled());
+            query.setParameter("rentedArea",contractDTO.getRentedArea());
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+
+        }catch(Exception e){
+            if(session.getTransaction() != null) session.getTransaction().rollback();
+        }
+    }
+}
